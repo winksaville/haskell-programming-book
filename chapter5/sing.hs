db47b3ef774610b7b567b0590b37fcf6d4fdcf90
Wink Saville <wink@saville.com>
2017-10-12 10:00:18 -0700

    Chapter 5 fix using case expression

--- chapter5/sing.hs
+++ chapter5/sing.hs
@@ -7,6 +7,7 @@ sndString :: [Char] -> [Char]
 sndString x = x ++ " over the rainbow"
 
 sing :: [Char] -> [Char]
-sing "Singin" = fstString "Singin"
-sing "Somewhere" = sndString "Somewhere"
-sing _ = "I don't know the song"
+sing firstWord = case firstWord of
+    "Singin" -> fstString firstWord
+    "Somewhere" -> sndString firstWord
+    otherwise -> "I don't know the song"
