5c206521e05f4d1506a931f68e13f00b1a9b9564
Wink Saville <wink@saville.com>
2017-10-13 14:48:17 -0700

    Add Sentence type class
    This did type check, but s1 is only partially applied so to use s1 you
    need a parameter.
    
    > s1 "alot"
    Sentence "dogs" "drool" "alot"
    > s2
    Sentence "Julie" "loves" "dogs"

--- chapter6/doesItTypeCheck.hs
+++ chapter6/doesItTypeCheck.hs
@@ -9,3 +9,14 @@ data Mood = Blah
           | Woot deriving (Eq, Show)
 
 settleDown x = if x == Woot then Blah else x
+
+type Subject = String
+type Verb = String
+type Object = String
+
+data Sentence =
+    Sentence Subject Verb Object
+    deriving (Eq, Show)
+
+s1 = Sentence "dogs" "drool"
+s2 = Sentence "Julie" "loves" "dogs"
