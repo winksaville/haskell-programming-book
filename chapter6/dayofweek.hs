caebd010b294b3ff01d48c7c21b9f1588a8f23d0
Wink Saville <wink@saville.com>
2017-10-13 10:34:38 -0700

    Add DayOfWeek and Date

---
+++ chapter6/dayofweek.hs
@@ -0,0 +1,22 @@
+module DayOfWeek where
+
+data DayOfWeek =
+    Mon | Tue | Wed | Thu | Fri | Sat | Sun deriving Show
+
+instance Eq DayOfWeek where
+    (==) Mon Mon = True
+    (==) Tue Tue = True
+    (==) Wed Wed = True
+    (==) Thu Thu = True
+    (==) Fri Fri = True
+    (==) Sat Sat = True
+    (==) Sun Sun = True
+    (==) _ _     = False
+
+data Date =
+    Date DayOfWeek Int deriving Show
+
+instance Eq Date where
+    (==) (Date weekDay dayOfMonth)
+         (Date weekDay' dayOfMonth') =
+            weekDay == weekDay' && dayOfMonth == dayOfMonth'
