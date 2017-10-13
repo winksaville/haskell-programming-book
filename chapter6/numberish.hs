4ea178d3759b76d49f7318c4c91660c18c96d98d
Wink Saville <wink@saville.com>
2017-10-13 14:11:59 -0700

    Add defaultNumber

--- chapter6/numberish.hs
+++ chapter6/numberish.hs
@@ -3,6 +3,7 @@ module Numberish where
 class Numberish a where
     fromNumber :: Integer -> a
     toNumber :: a -> Integer
+    defaultNumber :: a
 
 newtype Age =
     Age Integer
@@ -11,6 +12,7 @@ newtype Age =
 instance Numberish Age where
     fromNumber n = Age n
     toNumber (Age n) = n
+    defaultNumber = Age 65
 
 newtype Year =
     Year Integer
@@ -19,6 +21,7 @@ newtype Year =
 instance Numberish Year where
     fromNumber n = Year n
     toNumber (Year n) = n
+    defaultNumber = Year 1988
 
 sumNumberish :: Numberish a => a -> a -> a
 sumNumberish a a' = fromNumber summed
