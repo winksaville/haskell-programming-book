95c54555c820c99051cc52e76074ed79432de31b
Wink Saville <wink@saville.com>
2017-10-13 13:19:24 -0700

    Make Fri the greatest day

--- chapter6/dayofweek.hs
+++ chapter6/dayofweek.hs
@@ -1,7 +1,7 @@
 module DayOfWeek where
 
 data DayOfWeek =
-    Mon | Tue | Wed | Thu | Fri | Sat | Sun deriving Show
+    Mon | Tue | Wed | Thu | Fri | Sat | Sun deriving (Ord, Show)
 
 instance Eq DayOfWeek where
     (==) Mon Mon = True
@@ -12,6 +12,17 @@ instance Eq DayOfWeek where
     (==) Sat Sat = True
     (==) Sun Sun = True
     (==) _ _     = False
+
+-- Making Friday the Best Day of the week
+-- i.e. Fri is equal to Fri
+--      Fri is GT all other days
+--      All other days are LT Fri
+--      Any other day are always equal
+instance Ord DayOfWeek where
+    compare Fri Fri = EQ
+    compare Fri _ = GT
+    compare _ Fri = LT
+    compare _ _ = EQ
 
 data Date =
     Date DayOfWeek Int deriving Show
