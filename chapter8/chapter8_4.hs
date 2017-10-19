6a8e547ed61e5927a3822ed19fa6afcf010898de
Wink Saville <wink@saville.com>
2017-10-19 14:30:40 -0700

    Chapter 8.4 add fibFast and wrap in main
    fibFast/fibs is from here:
     http://blog.srinivasan.biz/software/fibonacci-numbers-the-slow-way-or-the-fast-and-lazy-way
    
    Rename fib to fibSlow

--- chapter8/chapter8_4.hs
+++ chapter8/chapter8_4.hs
@@ -1,10 +1,10 @@
-module FibonacciNumbers where
+import System.Environment
 import Debug.Trace (trace, traceShow)
 
-fib :: Integral a => a -> a
-fib 0 = 0
-fib 1 = 1
-fib n = fib(n - 1) + fib(n - 2)
+fibSlow :: Integral a => a -> a
+fibSlow 0 = 0
+fibSlow 1 = 1
+fibSlow n = fibSlow(n - 1) + fibSlow(n - 2)
 
 fib' :: (Show a, Integral a) => a -> a
 fib' 0 = trace "result fib'(0)=0" 0
@@ -16,3 +16,18 @@ fib' n =
         result = trace ("nless1=" ++ show nless1 ++ " nless2=" ++ show nless2)
                     (nless1 + nless2)
     in  trace ("result fib'(" ++ show param ++ ")=" ++ show result) result
+
+-- Here is a fast version from here:
+-- http://blog.srinivasan.biz/software/fibonacci-numbers-the-slow-way-or-the-fast-and-lazy-way
+fibs :: [Integer]
+fibs = 1:1:zipWith (+) fibs (tail fibs)
+
+fibFast :: Int -> Integer
+fibFast n = last $ take n fibs
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let count = read $ head args
+    print $ fibFast count
+    if count <= 35 then print (fibSlow count) else putStrLn "max for fib is 35"
