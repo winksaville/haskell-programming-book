3107f3145eedbde7e9200cfb7902327bbbbae856
Wink Saville <wink@saville.com>
2017-11-07 12:35:45 -0800

    Add Identity Crisis and Arbitrary Products

--- chapter14/chapter14.6-ArbitraryInstances.hs
+++ chapter14/chapter14.6-ArbitraryInstances.hs
@@ -10,7 +10,38 @@ trivialGen = return Trivial
 instance Arbitrary Trivial where
     arbitrary = trivialGen
 
+-- Identity Crisis
+data Identity a = Identity a deriving (Eq, Show)
+
+identityGen :: Arbitrary a => Gen (Identity a)
+identityGen = do
+    a <- arbitrary
+    return (Identity a)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+    arbitrary = identityGen
+
+identityGenInt :: Gen (Identity Int)
+identityGenInt = identityGen
+
+-- Arbitrary Products
+data Pair a b = Pair a b deriving (Eq, Show)
+
+pairGen :: (Arbitrary a, Arbitrary b) => Gen (Pair a b)
+pairGen = do
+    a <- arbitrary
+    b <- arbitrary
+    return (Pair a b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Pair a b) where
+    arbitrary = pairGen
+
+pairGenIntString :: Gen (Pair Int String)
+pairGenIntString = pairGen
+
 
 main :: IO ()
 main = do
     sample trivialGen
+    sample identityGenInt
+    sample pairGenIntString
