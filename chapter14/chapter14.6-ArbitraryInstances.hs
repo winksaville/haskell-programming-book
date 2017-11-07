e770b2abb779155e2b7c28ea709981094465cba4
Wink Saville <wink@saville.com>
2017-11-07 12:08:24 -0800

    Add chapter14.6-ArbitraryInstance.hs
    Implement trivialGen the most trivial generator and
    an instance of arbitrary which is trivialGen

---
+++ chapter14/chapter14.6-ArbitraryInstances.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import Test.QuickCheck
+
+data Trivial = Trivial deriving (Eq, Show)
+
+trivialGen :: Gen Trivial
+trivialGen = return Trivial
+
+instance Arbitrary Trivial where
+    arbitrary = trivialGen
+
+
+main :: IO ()
+main = do
+    sample trivialGen
