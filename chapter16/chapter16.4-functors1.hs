6453426336536865a63e3b8cf65628489f373f06
Wink Saville <wink@saville.com>
2017-11-10 17:01:25 -0800

    Fix white space

--- chapter16/chapter16.4-functors1.hs
+++ chapter16/chapter16.4-functors1.hs
@@ -4,9 +4,9 @@ instance Functor FixMePls where
     fmap = error "it doesn't matter, it won't compile"
 
 --Fail compiling:
---  $ ghc chapter16.4-functors1.hs 
+--  $ ghc chapter16.4-functors1.hs
 --  [1 of 1] Compiling Main             ( chapter16.4-functors1.hs, chapter16.4-functors1.o )
---  
+--
 --  chapter16.4-functors1.hs:3:18: error:
 --      • Expected kind ‘* -> *’, but ‘FixMePls’ has kind ‘*’
 --      • In the first argument of ‘Functor’, namely ‘FixMePls’
